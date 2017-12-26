c0b3489fac7989830a562268cbe96ceca634d1e2
piercebelton <piercebelton@gmail.com>
2017-12-26 11:54:56 -0800

    Finish ch3

--- Chapter 3/DailyRate Using Optional Parameters/DailyRate/Program.cs
+++ Chapter 3/DailyRate Using Optional Parameters/DailyRate/Program.cs
@@ -15,7 +15,31 @@ namespace DailyRate
 
         void run()
         {
-
+            double fee = calculateFee(theDailyRate : 375.0);
+            Console.WriteLine($"Fee is {fee}");
         }
+
+        private double calculateFee(double dailyRate = 500.0, int noOfDays = 1)
+        {
+            Console.WriteLine("calculateFee using two optional parameters");
+            return dailyRate * noOfDays;
+        }
+
+        private double calculateFee(double theDailyRate = 500.0)
+        {
+            Console.WriteLine("caluclateFee using one optional parameter");
+            int defaultNoOfDays = 1;
+            return theDailyRate * defaultNoOfDays;
+        }
+
+        private double calculateFee()
+        {
+            Console.WriteLine("calculateFee using hard coded values");
+            double defaultDailyRate = 400.0;
+            int defaultNoOfDays = 1;
+            return defaultDailyRate * defaultNoOfDays;
+        }
+
+
     }
 }
