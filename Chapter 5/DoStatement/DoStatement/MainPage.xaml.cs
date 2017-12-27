c7bc8d1e447b7b14468a6356ed6e63a3cddbbfe4
piercebelton <piercebelton@gmail.com>
2017-12-26 17:09:44 -0800

    finished ch5

--- Chapter 5/DoStatement/DoStatement/MainPage.xaml.cs
+++ Chapter 5/DoStatement/DoStatement/MainPage.xaml.cs
@@ -29,7 +29,20 @@ namespace DoStatement
 
         private void showStepsClick(object sender, RoutedEventArgs e)
         {
+            int amount = int.Parse(number.Text);
+            steps.Text = "";
+            string current = "";
 
+            do
+            {
+                int nextDigit = amount % 8;
+                amount /= 8;
+                int digitCode = '0' + nextDigit;
+                char digit = Convert.ToChar(digitCode);
+                current = digit + current;
+                steps.Text += current + "\n";
+            }
+            while (amount != 0);
         }
     }
 }
