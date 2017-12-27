614d6157ecc45e45b457050e91dee6a7d8c6a87c
piercebelton <piercebelton@gmail.com>
2017-12-26 16:42:47 -0800

    ch5 while loops

--- Chapter 5/WhileStatement/WhileStatement/MainPage.xaml.cs
+++ Chapter 5/WhileStatement/WhileStatement/MainPage.xaml.cs
@@ -50,7 +50,15 @@ namespace WhileStatement
 
         private void displayData(TextReader reader)
         {
-            // TODO: add while loop here
+            source.Text = "";
+            string line = reader.ReadLine();
+
+            while (line != null)
+            {
+                source.Text += line + '\n';
+                line = reader.ReadLine();
+            }
+            reader.Dispose();
 
         }
     }
