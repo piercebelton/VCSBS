811595fc2222ded493cfe4c1652eb8b8b8590f2a
piercebelton <piercebelton@gmail.com>
2018-01-09 10:48:33 -0800

    finish drawing using interfaces ch13

--- Chapter 13/Drawing/Drawing/DrawingPad.xaml.cs
+++ Chapter 13/Drawing/Drawing/DrawingPad.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +26,44 @@ namespace Drawing
         public DrawingPad()
         {
             this.InitializeComponent();
+        }
+
+        private void drawingCanvas_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Point mouseLocation = e.GetPosition(this.drawingCanvas);
+            Square mySquare = new Square(100);
+
+            if (mySquare is IDraw)
+            {
+                IDraw drawSquare = mySquare;
+                drawSquare.SetLocation((int)mouseLocation.X, (int)mouseLocation.Y);
+                drawSquare.Draw(drawingCanvas);
+            }
+
+            if (mySquare is IColor)
+            {
+                IColor colorSquare = mySquare;
+                colorSquare.SetColor(Colors.BlueViolet);
+            }
+        }
+
+        private void drawingCanvas_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            Point mouseLocation = e.GetPosition(this.drawingCanvas);
+            Circle myCircle = new Circle(100);
+
+            if (myCircle is IDraw)
+            {
+                IDraw drawCircle = myCircle;
+                drawCircle.SetLocation((int)mouseLocation.X, (int)mouseLocation.Y);
+                drawCircle.Draw(drawingCanvas);
+            }
+
+            if (myCircle is IColor)
+            {
+                IColor colorCircle = myCircle;
+                colorCircle.SetColor(Colors.HotPink);
+            }
         }
     }
 }
